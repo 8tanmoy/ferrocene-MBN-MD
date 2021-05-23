c7ca92e3f4189898e7fed72fede3c45d69cfa3c3
Tanmoy Pal <tanmoy@scc2.bu.edu>
2021-05-23 16:36:48 -0400

    plot

---
+++ plot/change_names.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+name=XXXX
+f1=field_inter_mbn.dat
+f2=field_inter_nombn.dat
+f3=field_mbn.dat
+f4=field_lig.dat
+f5=field_sol.dat
+mv $f1 ${name}_$f1
+mv $f2 ${name}_$f2
+mv $f3 ${name}_$f3
+mv $f4 ${name}_$f4
+mv $f5 ${name}_$f5
+
